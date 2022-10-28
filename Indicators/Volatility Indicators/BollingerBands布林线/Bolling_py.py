85938833760540bfeace6d23cbc24cff2260eded
MaXNIYU <93212015+MaXNIYU@users.noreply.github.com>
2022-10-28 09:29:45 +0800

    Delete Bolling_py.py

--- Indicators/Volatility Indicators/BollingerBands布林线/Bolling_py.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import ta
-
-hband=ta.volatility.BollingerBands(r.pr['cl'],int(period),int(factor)).bollinger_hband_indicator()
-lband=ta.volatility.BollingerBands(r.pr['cl'],int(period),int(factor)).bollinger_lband_indicator()
