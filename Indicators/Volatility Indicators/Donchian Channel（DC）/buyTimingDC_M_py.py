b1de5812e3c8862f21266dcd45b23dcd007f0fde
MaXNIYU <93212015+MaXNIYU@users.noreply.github.com>
2022-11-01 20:44:52 +0800

    Delete buyTimingDC_M_py.py

--- Indicators/Volatility Indicators/Donchian Channel（DC）/buyTimingDC_M_py.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import ta
-
-#hband=ta.volatility.DonchianChannel(r.pr['hp'],r.pr['lp'],r.pr['cl'],int(period)).donchian_channel_hband()
-#lband=ta.volatility.DonchianChannel(r.pr['hp'],r.pr['lp'],r.pr['cl'],int(period)).donchian_channel_lband()
-mband=ta.volatility.DonchianChannel(r.pr['hp'],r.pr['lp'],r.pr['cl'],int(period)).donchian_channel_mband()
