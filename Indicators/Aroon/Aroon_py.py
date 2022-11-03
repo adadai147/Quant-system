a2fd19b8ec9aec9428b37d22e31869a192df62a6
TARKII <94103899+TARKII@users.noreply.github.com>
2022-11-03 10:24:18 +0800

    Delete Indicators/Aroon directory

--- Indicators/Aroon/Aroon_py.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import ta
-cl=r.pr['cl']
-n=int(n)
-aroon_down = ta.trend.AroonIndicator(cl,n).aroon_down()
-aroon_up = ta.trend.AroonIndicator(cl,n).aroon_up()
-aroon_indicator = ta.trend.AroonIndicator(cl,n).aroon_indicator()
