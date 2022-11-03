163760160753aa2591ee373d041d9299985b08af
MaXNIYU <93212015+MaXNIYU@users.noreply.github.com>
2022-11-03 18:04:20 +0800

    Add files via upload

---
+++ Indicators/Volatility Indicators/Donchian Channel（DC）/SMA/buyTimingDC_SMA.R
@@ -0,0 +1,132 @@
+library(reticulate)
+library(pacman)
+p_load(data.table,tidyverse,TTR,magrittr,parallel,plyr,gcookbook,ggplot2,TTR)
+
+
+##=== 准备运行策略所需的数据 =========
+pr <- fread('HQfd.csv') # 读取深证ETF 原始数据(优矿基金数据下载)
+pr <- pr[,.(date=tradeDate,cl=closePrice,
+            op=openPrice,hp=highestPrice,
+            lp=lowestPrice,adj=accumAdjFactor)] # 裁出所需数据
+pr <- pr[,c('cl','op','hp','lp'):={
+  list(cl*adj,op*adj,hp*adj,lp*adj)
+}]
+# 将价格转为后复权价格，保证计算的一致性
+
+
+library(TTR)
+
+Weekday <- c("星期一","星期二","星期三","星期四","星期五")
+
+#=======================基于DC与SMA的择时策略======================
+#1.m周期移动平均线与唐奇安中心线在n周期移动平均线的中心线以上交叉并收盘时，买入股票
+#2.m周期移动平均线与唐奇安中心线在n周期移动平均线的中心线以下交叉并收盘时，卖出股票
+#超参：m,n,period
+#=====================ATR(Average True Range)====================
+
+
+buyTimingDC_SMA <- function(x,hyperPars=c(m,n,p)){
+  py$period=hyperPars[3]
+  py_run_file("C:/量化投资/2022.10-11/Donchian Channel（DC）/SMA/buyTimingDC_SMA_py.py")
+  stg <- x[,{
+    MA_m <- SMA(x$op,hyperPars[1])        #计算出m日的移动平均值
+    MA_n <- SMA(x$op,hyperPars[2])        #计算出n日的移动平均值
+    mband<-py$mband
+    buySig<-ifelse((pr$cl>MA_m)&
+                     (((shift(MA_m,1)<mband)&(MA_m>mband))|
+                      ((shift(MA_m,1)>mband)&(MA_m<mband))),1,0)
+    sellSig<-ifelse((pr$cl<MA_m)&
+                     (((shift(MA_m,1)<mband)&(MA_m>mband))|
+                        ((shift(MA_m,1)>mband)&(MA_m<mband))),1,0)
+    
+    list(date,op,cl,buySig,sellSig)
+  }]
+  return(stg)
+}
+
+#策略函数测试
+#stgy <- buyTimingDC_SMA(x=pr,hyperPars=c(50,10,10))
+#stgy 
+
+
+
+#===============查准率评估策略函数 evaluateppv()=====================
+#输入策略，输出其PPV
+library("plyr")
+evaluateppv <- function(stgy){
+  loc<-(which(stgy[,'buySig']==1))
+  stgyb<-stgy[loc,]
+  
+  loc<-(which(stgy[,'sellSig']==1))
+  stgys<-stgy[loc,]
+  
+  list1 <- list()
+  list1[[1]] <- data.frame(t(stgyb$op))
+  list1[[2]] <- data.frame(t(stgys$cl))
+  u <- rbind.fill(list1)
+  u <- t(u)
+  profit <- na.omit(log(u[,2])-log(u[,1])) #根据卖出当天收盘价与买入当天开盘价取对数后作差计算盈利
+  ppv <- length(profit[profit>0])/length(profit)  #计算查准率
+  
+  return(ppv)
+}
+
+
+
+#超参：m,n,p
+m <- seq(50,100,by=1)
+n <- seq(10,30,by=1)
+p <- seq(10,30,by=1)
+
+parSpace <- expand_grid(m,n,p) %>% data.table #生成参数搜索空间，一行对应一套参数
+
+#=============进行网格搜索的Search()函数，输出标定指标最优策略==========
+#输入超参组合，输出最优组合
+Search <-function(parSpace){
+  Search <- parSpace[,{
+    cat('m=',m,' n=',n,' p=',p,'\n')
+    ppv <- evaluateppv(buyTimingDC_SMA(x=pr,hyperPars=c(m=m,n=n,p=p)))
+    ppv
+  },by=.(m,n,p)] #每个参数组合都检验，能暴力选出ppv最好的参数组合
+  setnames(Search,'V1','ppv')
+  Search  #查看参数组合以及其所对应的ppv值
+  Best <- Search[which.max(ppv)]
+  return(Best)    #查看最优参数组合s
+}
+Search(parSpace)
+
+
+
+#================= 使用 CSCV 计算 PBO ==================================
+hyperParSpace <- parSpace
+S=10L  #分十组
+
+fgs <- combn(S,floor(S/2),simplify = F) # 模型训练集的分组序号组合
+fulldata <- pr %>% mutate(group=cut(1:nrow(pr),S) %>% as.integer()) #在标记买卖信号的全数据上加上组标签
+
+X <- hyperParSpace[,{
+  cat('m=',m,' n=',n,' p=',p,'\n')
+  stg <-buyTimingDC_SMA(x=pr,
+                     hyperPars=c(m=m,n=n,p=p)) #首先在全数据上生成买入卖出信号
+  fulldata <- stg %>% mutate(group=cut(1:nrow(pr),S) %>% as.integer()) #在标记买卖信号的全数据上加上组标签
+  J.tubes <-fgs %>% map(~fulldata[group %in% .,]) # 生成含有所有训练集组合的列表
+  Jc.tubes <- J.tubes %>% map(~ setdiff(fulldata,.)) #用差集的方式生成含有所有验证集组合的列表
+  ppv.J <- map_dbl(J.tubes,~evaluateppv(.))
+  ppv.Jc <- map_dbl(Jc.tubes,~evaluateppv(.))
+  list(N=1:length(J.tubes),ppv.J=ppv.J,ppv.Jc=ppv.Jc)#不同N代表不同的训练集选择
+},by=.(m,n,p)] # 针对每一个超参组合，计算全部训练集J和对应的全部验证集Jc上的ppv结果
+
+#write.csv(X,"outcome.csv") #将训练结果写入csv文件
+
+Ld <- X[,{
+  fg <- which.max(ppv.J) # 确定训练集中最大的ppv对应的行号
+  w <- ppv.Jc %>% percent_rank() %>% `[`(fg)
+  w <- ifelse(w==1,0.999,ifelse(w==0,0.001,w))
+  lambda <- log(w/(1-w))
+  list(lambda=lambda)         
+},by=.(m,n,p)]# 针对每一个训练集，计算样本外测试集的ppv相对排名，并计算lambda
+
+write.csv(Ld,"Rank.csv") #将排名结果写入csv文件
+
+Fn.cdf <- Ld$lambda %>% ecdf #构建lambda的经验分布函数
+PBO <- Fn.cdf(0) # 根据经验分布函数，得到过拟合概率
